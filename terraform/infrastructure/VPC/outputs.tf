04941ed6297ca22c740f22140b3d6559b266ece0
DamirTesnjak <damir.tesjak@gmail.com>
2025-09-15 02:50:40 +0200

    writing terraform files (in progress)

--- terraform/infrastructure/VPC/outputs.tf
+++ terraform/infrastructure/VPC/outputs.tf
@@ -39,10 +39,6 @@ output "ecs_subnets" {
   ]
 }
 
-output "security_group_ecs_id" {
-  value = module.securityGroup.security_group_ecs_id
-}
-
 output "security_group_alb_id" {
   value = module.securityGroup.security_group_alb_id
 }
